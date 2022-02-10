573278108be9bdbb5025f21e2f4d4ae1e7651701
82107 <82107@DESKTOP-G1GEJ2I>
2022-02-10 23:38:57 +0900

    commentNo, replyId 시퀀스 사용 성공

--- src/main/java/model/service/CommentMapperRepositoryTest.java
+++ src/main/java/model/service/CommentMapperRepositoryTest.java
@@ -24,8 +24,8 @@ public class CommentMapperRepositoryTest {
 		User user1 = userMan.findUser(1);
 				
 		try {
-//			commentMan.insertComment(20200004L, 1, "제목1", "내용1", 1);
-//			commentMan.insertComment(20200005L, 2, "제목2", "내용2", 2);
+//			commentMan.insertComment(20200004L, 1, "�젣紐�1", "�궡�슜1", 1);
+//			commentMan.insertComment(20200005L, 2, "�젣紐�2", "�궡�슜2", 2);
 //			System.out.println();
 			
 //			commentMan.selectCommentByPrimaryKey(20200004L);
@@ -33,7 +33,7 @@ public class CommentMapperRepositoryTest {
 //			commentMan.selectCommentByCondition(2);
 //			System.out.println();
 	
-//			commentMan.updateComment(20200004L, "내용1바꿈");
+//			commentMan.updateComment(20200004L, "�궡�슜1諛붽퓞");
 //			commentMan.selectCommentByPrimaryKey(20200004L);
 //			System.out.println();
 			
@@ -41,11 +41,17 @@ public class CommentMapperRepositoryTest {
 //			commentMan.selectCommentByPrimaryKey(20200004L);
 //			System.out.println();		
 			
-//			commentMan.insertReply(1, 20200004L, 3, "20200004L번 게시물에 3번이 댓글달기");
-//			commentMan.insertReply(2, 20200004L, 1, "20200004L번 게시물에 1번이 댓글달기");
-//			commentMan.insertReply(3, 20200005L, 3, "20200005L번 게시물에 3번이 댓글달기");
+//			commentMan.insertReply(1, 20200004L, 3, "20200004L踰� 寃뚯떆臾쇱뿉 3踰덉씠 �뙎湲��떖湲�");
+//			commentMan.insertReply(2, 20200004L, 1, "20200004L踰� 寃뚯떆臾쇱뿉 1踰덉씠 �뙎湲��떖湲�");
+//			commentMan.insertReply(3, 20200005L, 3, "20200005L踰� 寃뚯떆臾쇱뿉 3踰덉씠 �뙎湲��떖湲�");
 
 //			commentMan.deleteAllReplies();
+			
+//			commentMan.insertComment(20200004L, 1, "제목1", "내용1", 1);
+//			commentMan.insertComment(20200005L, 2, "제목2", "내용2", 2);
+			
+//			commentMan.insertReply(20200444L, 1, 3, "1번 게시물에 유저 3번이 댓글달음");
+//			commentMan.insertReply(20200444L, 2, 2, "2번 게시물에 유저 2번이 댓글달음");
 		} finally {
 //			manager.deleteAllComments();
 //			manager.selectCommentByPrimaryKey(20200004L);
